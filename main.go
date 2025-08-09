1f78466ee950447f7e0a682459cc487efc55cd7a
nikita <nikita.red638@gmail.com>
2025-08-10 02:29:12 +0300

    feat: fetch emotes from specified guilds on start

--- main.go
+++ main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"emote-counter/internal/database"
 	"emote-counter/internal/handlers"
+	"emote-counter/internal/utils"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
+	"gorm.io/gorm/clause"
 )
 
 func main() {
@@ -19,6 +23,7 @@ func main() {
 	}
 
 	token := os.Getenv("TOKEN")
+	guilds := os.Getenv("GUILDS")
 
 	sess, err := discordgo.New("Bot " + token)
 	if err != nil {
@@ -37,6 +42,35 @@ func main() {
 	handlers.EventHandler(sess)
 
 	database.Init()
+
+	guildIds := strings.Split(guilds, ",")
+	for _, guild := range guildIds {
+		emotes, err := utils.GetGuildEmotes(sess, guild)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		var emoteCounts []database.EmoteCount
+		for _, emote := range emotes {
+			emoteCounts = append(emoteCounts, database.EmoteCount{
+				GuildID:  guild,
+				ID:       emote.ID,
+				Name:     emote.Name,
+				Animated: emote.Animated,
+			})
+			log.Println(emote.Animated)
+		}
+
+		var added []database.EmoteCount
+		if len(emotes) > 0 {
+			database.DB.Clauses(clause.OnConflict{DoNothing: true}, clause.Returning{}).
+				Create(&emoteCounts).Scan(&added)
+		}
+		if len(added) > 0 {
+			log.Printf("Updated emotes for guild '%v'", guild)
+		}
+	}
 
 	log.Println("Logged as " + sess.State.User.Username + "#" + sess.State.User.Discriminator)
 
