5e116312acad612ddad5d4792b2c9d19c50c7bb0
nikita <nikita.red638@gmail.com>
2025-08-10 02:28:10 +0300

    feat: add animated field for EmoteCount model

--- internal/database/models.go
+++ internal/database/models.go
@@ -1,8 +1,9 @@
 package database
 
 type EmoteCount struct {
-	ID      string `gorm:"primaryKey"`
-	GuildID string `gorm:"primaryKey"`
-	Name    string
-	Count   int64 `gorm:"default:0"`
+	ID       string `gorm:"primaryKey"`
+	GuildID  string `gorm:"primaryKey"`
+	Name     string
+	Count    int64 `gorm:"default:0"`
+	Animated bool
 }
